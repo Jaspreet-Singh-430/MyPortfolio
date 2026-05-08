c02eedf290e759bca5e844da7b0a221a25e79742
Jaspreet-Singh-430 <jaspreet9322@gmail.com>
2026-05-08 15:21:42 +0530

    new project added

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -74,8 +74,17 @@ const portfolio = () => {
             liveDemoLink:"https://jaspreet-singh-430.github.io/RockPaperScissors/",
             sourceCodeLink:"https://github.com/Jaspreet-Singh-430/RockPaperScissors/tree/rpss"
         },
+         {
+            id:8,
+            title:"Note Taker",
+            image:NoteTaker,
+            description:" A simple note-taking app that allows users to create, edit, and delete notes.",
+            liveDemoLink:"https://note-taking-app-tan-ten.vercel.app/",
+            sourceCodeLink:"https://github.com/Jaspreet-Singh-430/NoteTakingApp"
+
+        },
         {
-            id:7,
+            id:9,
             title:"Tinder-Clone",
             image:TinderClone,
             description:"A modern full-stack dating application inspired by Tinder, built with React, Node.js, Express, MongoDB, and Socket.IO. Users can create profiles, upload photos, swipe through profiles, match with other users, and chat in real time.",
